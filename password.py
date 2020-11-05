4f5fe624db93384c0221f9683be615d3804565eb
Jtwopen <jtwopen@gmail.com>
2020-11-06 01:01:02 +0800

    password-retry-i>0

--- password.py
+++ password.py
@@ -1,6 +1,6 @@
 pwd = 'a123456' #密碼
 i = 3 #可輸入次數
-while True:
+while i > 0:
 	password = input('請輸入密碼')
 	if password == pwd:
 		print('登入成功')
@@ -8,6 +8,5 @@ while True:
 	else:
 		i = i - 1
 		print('密碼錯誤,還有',i,'次機會')
-		if i == 0:
-			break
-			
+	
+
